4501dff110b4d0cb51af48f582642aa88a934ae1
Nguyễn Anh Dũng <55435241+dungna29@users.noreply.github.com>
2020-11-13 13:02:22 +0700

    dungna

---
+++ PT1630A_UD_COM108/Bai_2.1_VongLap_While/Main.c
@@ -0,0 +1,75 @@
+﻿#include <stdio.h>
+/// <summary>
+/// VÒNG LẶP 
+/// Lặp đi lặp lại 1 hành động nào đó
+/// VÒNG LẶP WHILE
+/// </summary>
+/// <returns></returns>
+int main()
+{
+	//Sử dụng: wh + tab
+	// while (true) - Biểu thức giống như if hoặc Else If
+	// {
+	// 	Chỉ khi biểu thức true thì vòng lặp mới được thực hiện
+	//	Vòng lặp thì nên tìm điểm ngắt vòng lặp
+	//	Break, Goto, Continue về tìm hiểu thêm
+	// }
+
+	//Ví dụ 5:  Sử dụng Break
+	// int a = 0;
+	// while (a < 5)
+	// {
+	// 	printf("Chao cac ban den voi WHILE %d \n", a);
+	// 	a++;
+	// }
+
+	//Ví dụ 4: Đưa ra điều kiện ngắt vòng lặp
+	// Tìm ra các điểm ngắt vòng lặp cho hợp lý
+	int a = 0;
+	while (a < 10)
+	{
+		if (a == 5)
+		{
+			break;//Dùng để ngắt vòng lặp
+		}
+		printf("Chao cac ban den voi WHILE %d \n", a);
+		a++;
+		
+	}
+
+	//Ví dụ 3: //Vòng lặp này là vòng lặp vô hạn
+	// while (5 > 3 && 3 >1)//(5 > 3 && 3 >1) = True
+	// {
+	// 	printf("Khi while  = true");
+	// }
+
+
+	// Ví dụ 2:
+	// while (5>6)//(5>6) = Flase
+	// {
+	// 	//Nếu biểu thức False thì sẽ không chạy xuống đây
+	// 	printf("Co chay hay khong");
+	// }
+
+
+
+	//Ví dụ 1: Vòng lặp vô hạn
+	// while (1)
+	// {
+	// 	//Chương trình nhập số Lẻ Chẵn
+	// // Bước 1:
+	// 	int input = 0;
+	// 	//Bước 2:
+	// 	printf("Moi ban nhap so nguyen: ");
+	// 	scanf_s("%d", &input);
+	// 	if (input % 2 == 0)
+	// 	{
+	// 		printf("Chan \n");
+	// 	}
+	// 	else
+	// 	{
+	// 		printf("Le \n");
+	// 	}
+	// }
+	
+}
---
+++ PT1630A_UD_COM108/Bai_2.2_VongLap_Do_While/Main.c
@@ -0,0 +1,57 @@
+﻿#include <stdio.h>
+/// <summary>
+/// Vòng lặp Do While
+/// Tương tự như định nghĩa While
+/// Đối với Do While thì biểu thức While mà False thì sẽ chạy 1 lần.
+/// </summary>
+/// <returns></returns>
+int main()
+{
+	//Cách dùng: do + tab
+	// do
+	// { mở lệnh
+	// 	 Thực hành động ít nhất là 1 lần nếu biểu thức False
+	// } đóng lệnh
+	// while (true); Chỉ chạy khi biểu thức = true
+
+	// Chương trình hỏi bạn gái có thích mình không?
+	char c;
+	do
+	{
+		printf("Cau co thich to khong? c = Co - k = ko : ");
+		scanf_s("%c", &c);
+		getchar();		
+	} while (!(c == 'c')); 
+	printf("Ok");
+	//Cách 1:
+	// do
+	// {
+	// 	printf("Cau co thich to khong? c = Co - k = ko : ");
+	// 	scanf_s("%c", &c);
+	// 	getchar();
+	// 	if (c == 'c')
+	// 	{
+	// 		printf("Ok");
+	// 		break;
+	// 	}
+	// }
+	// while (1);
+	
+	//Ví dụ 1: Biểu thức  = False
+	// do
+	// {
+	// 	printf("While = False");
+	// }
+	// while (5 < 3);//(5 < 3) = False
+
+	/*
+	 * Bài 1: Viết 1 chương trình cho phép người dùng nhập vào từ 0 đến 100.
+	 * Nếu người dùng nhập không đúng yêu cầu thì yêu cầu người dùng nhập lại.
+	 *
+	 * Bài 2: Viết 1 chương trình cho phép người dùng chỉ nhập vào số chẵn.
+	 * Nếu số lẻ thông báo và bắt người dùng nhập lại.
+	 *
+	 * Bài 3: Viết 1 chương trình cho phép người dùng nhập vào 1 bảng cửu chương kiểu số nguyên
+	 * Sau đó in bảng cửu chương người dùng đã chỉ định ra màn hình. Sử dụng While hoặc Do While.
+	 */
+}
